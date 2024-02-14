09b4a3412aed8221caa565804690d90e71942c33
RoBruns <rodrigobrunsfh@gmail.com>
2024-02-14 19:22:12 -0300

    feat: create fields validations

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
+
 from clientes.models import Cliente
+from clientes.validators import (validate_celular, validate_cpf, validate_nome,
+                                 validate_rg)
+
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+
+    def validate(self, data):
+        if not validate_cpf(data['cpf']):
+            raise serializers.ValidationError({'cpf': 'O CPF deve ter 11 dígitos'}) # noqa
+
+        if not validate_rg(data['rg']):
+            raise serializers.ValidationError({'rg': 'O RG deve ter 9 dígitos'}) # noqa
+
+        if not validate_celular(data['celular']):
+            raise serializers.ValidationError({'celular': 'O celular deve ter 14 dígitos'}) # noqa
+
+        if not validate_nome(data['nome']):
+            raise serializers.ValidationError({'nome': 'Não inclua números neste campo'}) # noqa
+
+        return data
--- setup/settings.py
+++ setup/settings.py
@@ -82,22 +82,24 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa
     },
 ]
 
